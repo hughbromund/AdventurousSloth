8d1c94ae49ca32adb7b0f8dda60d8f4a79829cd2
Aditya Naik <adityan9900@gmail.com>
2020-01-25 01:54:44 -0500

    Cleaned up project organization

--- frontend/adventurous-app/src/index.js
+++ frontend/adventurous-app/src/index.js
@@ -1,40 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { HashRouter as Router, Route, Switch, Link } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import "./index.css";
-import FullPage from "./FullPage";
-import PageContain from "./PageContain";
+ReactDOM.render(<App />, document.getElementById('root'));
 
-import "./index.css";
-
-class Demo extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1 className="title">React Page Scroller Demo</h1>
-        <div className="links">
-          <div className="link">
-            <Link to="/fullpage">Full page demo</Link>
-          </div>
-          <div className="link">
-            <Link to="/contain">Page contain demo</Link>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(
-  <Router basename="/demos">
-    <div>
-      <Switch>
-        <Route exact path="/" component={Demo} />
-        <Route path="/fullpage" component={FullPage} />
-        <Route path="/contain" component={PageContain} />
-      </Switch>
-    </div>
-  </Router>,
-  document.getElementById("root"),
-);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
